ccf652b7f96cdc97a71daa845a080c57e3f40edd
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2019-11-07 16:20:01 +0100

    Prepare rails app with external specs

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,52 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = Dose.all.where(cocktail_id: @cocktail)
+    # raise
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Lemon")
+Ingredient.create(name: "Mint Leaves")
+Ingredient.create(name: "Strawberries")
+Ingredient.create(name: "Salt")
+Ingredient.create(name: "Sugar")
+Ingredient.create(name: "Egg")
+Ingredient.create(name: "Coca Cola")
+Ingredient.create(name: "Sprite")
+Ingredient.create(name: "Lemon Soda")
+Ingredient.create(name: "Tonic")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Whiskey")
+Ingredient.create(name: "Dry Vermouth")
+Ingredient.create(name: "Brandy")
+
+Cocktail.create(name: "Vodka Tonic")
+Cocktail.create(name: "Gin Tonic")
+Cocktail.create(name: "Old Fashioned")
+Cocktail.create(name: "Sex on the Beach")
+
