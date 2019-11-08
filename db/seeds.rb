593b4618be8f8f3b02f464a9961a7170520bcc37
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2019-11-08 18:32:27 +0100

    seed photo fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,8 +26,39 @@ Ingredient.create(name: "Whiskey")
 Ingredient.create(name: "Dry Vermouth")
 Ingredient.create(name: "Brandy")
 
-Cocktail.create(name: "Vodka Tonic")
-Cocktail.create(name: "Gin Tonic")
-Cocktail.create(name: "Old Fashioned")
-Cocktail.create(name: "Sex on the Beach")
+url = "https://www.thespruceeats.com/thmb/9F_NJscxiWr_cBIJG0PGPGa6YHc=/2048x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/apple-martini-recipe-759579-6878_preview-5b241717a474be0036f49406.jpeg"
+applemartini = Cocktail.new(name: 'Apple Martini')
+applemartini.remote_photo_url = url
+applemartini.save
+
+url = "https://www.thespruceeats.com/thmb/Ngz6MOAhB48WpVw5bDTIAmgYS7A=/3849x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/vodka-martini-recipe-760983-8-5b48ffe1c9e77c001a664f6e.jpg"
+vodkamartini = Cocktail.new(name: 'Vodka Martini')
+vodkamartini.remote_photo_url = url
+vodkamartini.save
+
+url = "https://www.bbcgoodfood.com/sites/default/files/editor_files/2019/10/negroni.jpg"
+negroni = Cocktail.new(name: 'Negroni')
+negroni.remote_photo_url = url
+negroni.save
+
+url = "https://www.bbcgoodfood.com/sites/default/files/editor_files/2019/10/bramble.jpg"
+bramble = Cocktail.new(name: 'Bramble')
+bramble.remote_photo_url = url
+bramble.save
+
+url = "https://www.bbcgoodfood.com/sites/default/files/editor_files/2019/10/elderflower-collins.jpg"
+edelflower = Cocktail.new(name: 'Edelflower Collins')
+edelflower.remote_photo_url = url
+edelflower.save
+
+url = "https://www.bbcgoodfood.com/sites/default/files/editor_files/2019/10/gimlet.jpg"
+gimblet = Cocktail.new(name: 'Gimblet')
+gimblet.remote_photo_url = url
+gimblet.save
+
+url = "https://www.bbcgoodfood.com/sites/default/files/editor_files/2019/10/gin-and-tonic-700-350.jpg"
+gt = Cocktail.new(name: 'G&T')
+gt.remote_photo_url = url
+gt.save
+
 
